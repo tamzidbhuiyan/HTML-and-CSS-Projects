92e7c496b77e07209c35142f5a7dc4e7c65f0118
tamzidbhuiyan <tamzidbhuiyan@gmail.com>
2022-01-26 01:32:35 +0000

    Update Basic_JavaScript_1.js

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,6 +1,7 @@
-<!DOCTYPE HTML>
-<html>
 <script>
-alert("Hello, world");
+Function My_First_Function() {
+	var String = "Hello world";
+	var result = String.fontcolor("green");
+	document.getElementById("Irish").innerHTML = result;
+}
 </script>
-</html>
